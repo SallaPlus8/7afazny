ee8e6563a76665faee81a340043d10620b06c961
mahmood2002reda <111697516+mahmood2002reda@users.noreply.github.com>
2024-11-03 09:51:14 +0200

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
-Route::get('/', function () {
 
-    return view('dashboard');
-});
 Route::group(['prefix' => LaravelLocalization::setLocale()], function()
 {
     // Route::get('/', 'HomeController@index');
 
-    // Route::get('/', function () {
-    //     return view('dashboard');
-    // });
+     Route::get('/', function () {
+        return view('dashboard');
+    });
     Route::get('/login', function () {
         return view('auth.login');
     });
